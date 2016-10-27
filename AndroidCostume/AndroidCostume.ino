cbb28b3a3131f43d72442f3e66880138d46cc599
noahrondeau <ntrondeau@gmail.com>
2016-10-26 22:10:28 -0700

    Added AndroidCostume

---
+++ AndroidCostume/AndroidCostume.ino
@@ -0,0 +1,25 @@
+void setup()
+{
+  enum pin
+  {
+    p2 = 2;
+    p3,
+    p4,
+    p5,
+    p6,
+    p7,
+    p8,
+    p9,
+    p10,
+    p11,
+    p12,
+    p13,
+  }
+
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+
+}
