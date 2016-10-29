06a4a1a88a537bfc49f00b0f3ee9710dd4160483
noahrondeau <ntrondeau@gmail.com>
2016-10-29 01:16:07 -0700

    Set up LED templating framework with protothreading

--- AndroidCostume/AndroidCostume.ino
+++ AndroidCostume/AndroidCostume.ino
@@ -1,7 +1,11 @@
-  
-  
-  
-enum pin
+
+#include <Thread.h>
+#include <ThreadController.h>
+
+#include <math.h>
+
+
+enum Pin_t
 {
   p2 = 2,
   p3,
@@ -17,19 +21,63 @@ enum pin
   p13
 };
 
+template< int pin > struct Functor_t
+{
+  Functor_t( void (&func)() )
+  {
+    m_func = func;
+  }
+  void operator()()
+  {
+    m_func();
+  }
+
+  void (&m_func)();
+};
+
+// LED toggle callback
+
+template<int pin> void ledToggle()
+{
+  digitalWrite(pin, !digitalRead(pin));
+}
+
+// LED pulse callbacks
+
+template<int pin, typename T, T functor> void ledPWM()
+{
+  functor();
+}
+
+template<int pin> void doSinusoid()
+{
+  static int counter = 0;
+}
+
+ThreadController ledController = ThreadController();
 
 void setup()
 {
+
+  // Set pinmode for all digital out pins
   for( int i = 2; i <= 13; i++ )
   {
     pinMode( i, OUTPUT );
     digitalWrite(i, LOW);
   }
-}
 
+  // LED THREADS
+  
+  // Turns stays on for one second, off for one second
+  Thread p2Thread = Thread();
+  p2Thread.setInterval(1000);
+  p2Thread.onRun(ledToggle<p2>);
+
+  // COnfigure led controller
+  ledController.add(&p2Thread);
+}
+  
 void loop()
 {
-  delay(500);
-  digitalWrite( p3, !digitalRead( p3 ));
-
+  ledController.run();
 }
